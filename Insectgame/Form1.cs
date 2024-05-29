ee17850aa04f9bd7de8b056db4e98ad5dc464cd5
Arek Jobs <arkjob060422@student.jenseneducation.se>
2024-05-29 10:32:15 +0200

    Add project files.

---
+++ Insectgame/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Insectgame
+{
+    // Base form class
+    public class BaseForm
+    {
+        // Attributes
+        public int Size { get; set; }
+        public int Hitpoints { get; set; }
+ 
+        // Senses attributes
+        public int Eyesight { get; set; }
+        public int Hearing { get; set; }
+        public int Feeling { get; set; }
+        public int Smell { get; set; }
+
+        public int Blunt_Damage { get; set; }
+        public int Slice_Damage { get; set; }
+        public int Resistance { get; set; }
+        public int Mobility { get; set; }
+
+        // Methods (lite hjälp från google och chat)
+        public virtual int SetSize() => 200;
+        public virtual int SetHitpoint() => 500;
+        public virtual int SetSenses(int eyesight, int hearing, int feeling, int smell)
+        {
+            Eyesight = eyesight;
+            Hearing = hearing;
+            Feeling = feeling;
+            Smell = smell;
+            return Senses;
+        }
+        // Property to get the combined senses value
+        public virtual int Senses => Eyesight + Hearing + Feeling + Smell;
+        
+        // Sets individual damages and returns the total damage
+        public virtual int SetDamage(int blunt_Damage, int slice_Damage)
+        {
+            Blunt_Damage = blunt_Damage;
+            Slice_Damage = slice_Damage;
+            return Damage;
+        }
+
+        // Property to get the combined damage value
+        public int Damage => Blunt_Damage + Slice_Damage;
+
+        public virtual int SetResistance() => 50;
+        public virtual int SetMobility() => 100;
+    }
+
+    // Insect class inheriting from BaseForm
+    public class Insect : BaseForm
+    {
+        // Attributes
+        public bool Exoskeleton { get; set; }
+        public bool Mandibles { get; set; }
+
+        // Methods
+        public bool SetExoskeleton() => true;
+        public bool ExoskeletonFunctionality() => Exoskeleton;
+        public bool SetMandibles() => true;
+    }
+
+    // Lone Insect class inheriting from Insect
+    public class LoneInsect : Insect
+    {
+        // Attributes
+        public bool HpRegen { get; set; }
+        public bool ThornyLegs { get; set; }
+
+        // Methods
+        public bool SootheRegen(bool time)
+        {
+            if (time)
+            {
+                HpRegen = true;
+                return true;
+            }
+            return false;
+        }
+
+        public void ThornyLegsFunctionality()
+        {
+            if (ThornyLegs)
+            {
+                Mobility += 20;
+                Blunt_Damage += 10;
+                Slice_Damage += 10;
+            }
+        }
+    }
+
+    // Queen class inheriting from Insect
+    public class Queen : Insect
+    {
+        // Attributes
+        public bool Reproduction { get; set; }
+        public bool RoyalCry { get; set; }
+
+        // Methods
+        public bool SelfReproductionFunction() => Reproduction;
+
+        public void RoyalCryFunctionality()
+        {
+            if (RoyalCry)
+            {
+                Feeling += 20;
+            }
+        }
+    }
+}
---
+++ Insectgame/Program1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Main program (Med hjälp från chatGPT)
+namespace Insectgame
+{
+    class Program1
+    {
+        static void Main(string[] args)
+        {
+            // Example usage
+            LoneInsect loneInsect = new LoneInsect
+            {
+                Size = 100,
+                Hitpoints = 100,
+                Resistance = 30,
+                Mobility = 20,
+                Exoskeleton = true,
+                Mandibles = true,
+                HpRegen = true,
+                ThornyLegs = true
+            };
+            loneInsect.SetSenses(10, 10, 10, 20);
+            loneInsect.SetDamage(15, 10);
+
+            Console.WriteLine("Lone Insect HP Regen: " + loneInsect.SootheRegen(true));
+            loneInsect.ThornyLegsFunctionality();
+            Console.WriteLine("Lone Insect Size after Thorny Legs Functionality: " + loneInsect.Size);
+            Console.WriteLine("Lone Insect Senses: " + loneInsect.Senses);
+            Console.WriteLine("Lone Insect Damage: " + loneInsect.Damage);
+            Console.WriteLine("Lone Insect Size: " + loneInsect.Size);
+            Console.WriteLine("Lone Insect Hitpoint: " + loneInsect.Hitpoints);
+            Console.WriteLine("Lone Insect Resistance: " + loneInsect.Resistance);
+            Console.WriteLine("Lone Insect Mobility: " + loneInsect.Mobility);
+            Console.WriteLine("Lone Insect Exoskeleton: " + loneInsect.Exoskeleton);
+            Console.WriteLine("Lone Insect Mandibles: " + loneInsect.Mandibles);
+            Console.WriteLine("Lone Insect HpRegen: " + loneInsect.HpRegen);
+            Console.WriteLine("Lone Insect ThornyLegs: " + loneInsect.ThornyLegs);
+
+            Queen queen = new Queen
+            {
+                Size = 20,
+                Hitpoints = 200,
+                Resistance = 60,
+                Mobility = 40,
+                Exoskeleton = true,
+                Mandibles = true,
+                Reproduction = true,
+                RoyalCry = true
+            };
+            queen.SetSenses(25, 25, 25, 25);
+            queen.SetDamage(30, 20);
+
+            queen.RoyalCryFunctionality();
+            Console.WriteLine("Queen Size after Royal Cry Functionality: " + queen.Size);
+            Console.WriteLine("Queen Senses: " + queen.Senses);
+            Console.WriteLine("Queen Damage: " + queen.Damage);
+            Console.WriteLine("Lone Insect Size: " + queen.Size);
+            Console.WriteLine("Lone Insect Hitpoint: " + queen.Hitpoints);
+            Console.WriteLine("Lone Insect Resistance: " + queen.Resistance);
+            Console.WriteLine("Lone Insect Mobility: " + queen.Mobility);
+            Console.WriteLine("Lone Insect Exoskeleton: " + queen.Exoskeleton);
+            Console.WriteLine("Lone Insect Mandibles: " + queen.Mandibles);
+            Console.WriteLine("Lone Insect HpRegen: " + queen.Reproduction);
+            Console.WriteLine("Lone Insect ThornyLegs: " + queen.RoyalCry);
+        }
+    }
+}
+
